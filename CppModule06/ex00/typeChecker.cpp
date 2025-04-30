040e2713483b71af3c72fec29381c81e56ca3b6c
Soufyane Marsi <smarsi@c3r2p2.1337.ma>
2025-04-30 16:05:44 +0100

    chore: handle -inf, nan and +inf in double.

--- CppModule06/ex00/typeChecker.cpp
+++ CppModule06/ex00/typeChecker.cpp
@@ -4,11 +4,7 @@ bool is_char(std::string literal)
 {
     if (literal.length() == 1)
     {
-        if (!isascii(literal[0]))
-            std::cout << "impossible\n";
-        else if (!isprint(literal[0]))
-            std::cout << "Non displayable\n";
-        else if (!isdigit(literal[0]))
+        if (!isdigit(literal[0]))
             return (true);
     }
     return (false);
@@ -22,12 +18,6 @@ bool is_int(std::string literal)
             continue;
         else if (!isdigit(literal[i]))
             return (false);
-    }
-    long long number = std::atoll( literal.c_str() );
-    if (number > INT_MAX || number < INT_MIN)
-    {
-        std::cout << "impossible\n";
-        return (false);
     }
     return (true);
 }
@@ -48,13 +38,13 @@ bool is_float(std::string literal)
 
 bool is_double(std::string literal)
 {
-    if (literal == "-inff" || literal == "+inff" ||literal == "nanf") return true;
+    if (literal == "-inf" || literal == "+inf" ||literal == "nan") return true;
     bool flag = false;
     for (size_t i = 0; i < literal.length() - 1; i++)
     {
         if (i == 0 && (literal[i] == '-' || literal[i] == '+')) continue;
         else if (!isdigit(literal[i]) && (literal[i] != '.' && !flag)) return (false);
-        else if (literal[i] == '.' && isdigit(literal[i + 1])) flag = true; // to check cases like 1.f (a number should be after .)
+        else if (literal[i] == '.' && isdigit(literal[i + 1])) flag = true; // to check cases like 1. (a number should be after .)
         else if(literal[i] == '.') return (false);
     }
     return (flag);
