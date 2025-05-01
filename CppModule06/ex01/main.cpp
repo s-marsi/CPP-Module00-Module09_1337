c7512cb8710c8626fc01cdfbcc003dc46394eeb8
Soufyane Marsi <smarsi@c3r3p8.1337.ma>
2025-05-01 12:37:48 +0100

    test: modify Serializer class tests.

--- CppModule06/ex01/main.cpp
+++ CppModule06/ex01/main.cpp
@@ -6,4 +6,5 @@ int main()
     uintptr_t dataAdd = Serializer::serialize(&data);
     Data *dataBack = Serializer::deserialize(dataAdd);
     std::cout << "----------------- " << dataBack->name << " - " << dataBack->age << "------------\n";
+    return (0);
 }
