97cac556b6f037bcd9c8768eb6c45f1d72175bdf
Soufyane Marsi <smarsi@e1r11p12.1337.ma>
2025-05-05 14:30:12 +0100

    chore: modify implementation.

--- CppModule08/ex00/easyfind.hpp
+++ CppModule08/ex00/easyfind.hpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 
 template <typename T> void easyfind(T arg1, int arg2) {
-    typename T::iterator it = find(arg1.begin(), arg1.end(), arg2);
+    typename T::iterator it = std::find(arg1.begin(), arg1.end(), arg2);
     if (it != arg1.end())
         std::cout << *it << " Is Present\n";
     else
