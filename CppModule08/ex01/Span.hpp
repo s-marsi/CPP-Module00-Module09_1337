fd96c5f0cc8120db3d486b004f88e661cca53297
Soufyane Marsi <smarsi@e1r11p12.1337.ma>
2025-05-05 09:46:30 +0100

    feat: Define Span class

---
+++ CppModule08/ex01/Span.hpp
@@ -0,0 +1,24 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+#include <iostream>
+#include <exception>
+#include <bits/stdc++.h>
+
+class Span
+{
+    private:
+        unsigned int _capacity;
+        unsigned int index;
+        std::vector<int> _numbers;
+        Span();
+    public:
+        Span(unsigned int N);
+        Span(Span &rhs);
+        Span &operator=(Span &rhs);
+        void addNumber(int number);
+        int shortestSpan();
+        int longestSpan();
+        void getNumbers();
+        ~Span();
+};
+#endif
