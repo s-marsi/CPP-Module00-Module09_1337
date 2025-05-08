c2c5c55fd3fcf493c6a34660e1d3b42690061e11
Soufyane Marsi <smarsi@e1r9p1.1337.ma>
2025-05-08 09:48:09 +0100

    test: more test for easifind.

--- CppModule08/ex00/main.cpp
+++ CppModule08/ex00/main.cpp
@@ -10,10 +10,24 @@ int main() {
             arr.push_back(3);
             arr.push_back(4);
             easyfind(arr, 5);
-        } catch (char const*e) {
-            std::cout << e;
-        } catch (...) {
-            std::cout << "vector error.\n";
+
+        } catch (std::exception &e) {
+            std::cout << e.what();
+        }
+    }
+
+    {
+        std::vector<int> arr;
+        try {
+            arr.push_back(0);
+            arr.push_back(1);
+            arr.push_back(2);
+            arr.push_back(3);
+            arr.push_back(4);
+            easyfind(arr, 0);
+            easyfind(arr, -5);
+        } catch (std::exception &e) {
+            std::cout << e.what();
         }
     }
     return (0);
