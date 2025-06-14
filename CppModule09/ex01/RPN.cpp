cf851eb13d733832aecb97d6be339f62c7b0c209
s-marsi <soufianemarsi1999@gmail.com>
2025-06-14 19:22:37 +0100

    feat: implement is_operator function that check if a char is in (+-*/) or not.

--- CppModule09/ex01/RPN.cpp
+++ CppModule09/ex01/RPN.cpp
@@ -2,7 +2,7 @@
 
 RPN::RPN() { }
 
-RPN::RPN(RPN &rhs) { *this = rhs }
+RPN::RPN(RPN &rhs) { *this = rhs; }
 
 RPN &RPN::operator=(RPN &rhs) { 
     if (this != &rhs) {
@@ -10,5 +10,34 @@ RPN &RPN::operator=(RPN &rhs) {
     }
     return (*this);
 }
+
+// int calculate(char &operation, int &left_operator, int &right_operator) {
+//     if (operation == '+')
+//         return (left_operator + right_operator);
+//     if (operation == '-')
+//         return (left_operator - right_operator);
+//     if (operation == '*')
+//         return (left_operator * right_operator);
+//     return (left_operator / right_operator);
+// }
+
+bool is_operator(std::string expr) {
+    std::string operators = "+-*/";
+    for (int i = 0; i < operators.length(); i++) {
+        if (operators[i] == expr[0])
+            return (true);
+    }
+    return (false);
+}
+
+// void RPN::evaluate(char *expr) {
+//     while (expr != NULL)
+//     {
+//         int left_operator;
+//         int right_operator;
+//         std::string current_expr = expr;
+//         expr = strtok(NULL, "-");
+//     }
+// }
 
 RPN::~RPN() { }
