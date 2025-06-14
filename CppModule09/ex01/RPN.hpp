92cf8552a07305688f62a09f5ccf84529dd389e8
s-marsi <soufianemarsi1999@gmail.com>
2025-06-14 19:23:05 +0100

    feat: add calculate prototype to the header file.

--- CppModule09/ex01/RPN.hpp
+++ CppModule09/ex01/RPN.hpp
@@ -7,7 +7,7 @@
 #include <stack>
 
 bool is_operator(std::string expr);
-// int  calculate(char &operation, int &left_operator, int &right_operator);
+int  calculate(char &operation, int &left_operator, int &right_operator);
 class RPN
 {
     private:
