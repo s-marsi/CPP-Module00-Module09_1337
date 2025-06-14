b8234838a9523664587fb6b9442d38aa619a4c25
s-marsi <soufianemarsi1999@gmail.com>
2025-06-14 19:21:58 +0100

    feat: add is_operator prototype to the header file.

--- CppModule09/ex01/RPN.hpp
+++ CppModule09/ex01/RPN.hpp
@@ -1,8 +1,13 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 #include <iostream>
+#include <stdio.h>
+
+#include <string.h>
 #include <stack>
 
+bool is_operator(std::string expr);
+// int  calculate(char &operation, int &left_operator, int &right_operator);
 class RPN
 {
     private:
@@ -11,6 +16,7 @@ class RPN
         RPN();
         RPN(RPN &rhs);
         RPN &operator=(RPN &rhs);
+        // void evaluate(char *expr);
         ~RPN();
 };
 
