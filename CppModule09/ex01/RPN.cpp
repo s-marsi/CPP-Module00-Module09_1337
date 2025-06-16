30bf90f4773aba469db5f9272cdef0c11268e6f8
Soufyane Marsi <smarsi@e1r12p3.1337.ma>
2025-06-16 15:25:10 +0100

    feat: implement evaluator methode to evaluate the expression

--- CppModule09/ex01/RPN.cpp
+++ CppModule09/ex01/RPN.cpp
@@ -18,26 +18,53 @@ int calculate(char &operation, int &left_operator, int &right_operator) {
         return (left_operator - right_operator);
     if (operation == '*')
         return (left_operator * right_operator);
-    return (left_operator / right_operator);
+    else if (operation == '/') {
+        if (right_operator == 0) {
+                std::cout << "Error\n";
+                std::cout << "Division by zero\n";
+                exit (1);
+        }
+        return (left_operator / right_operator);
+    }
+    return (0);
 }
 
 bool is_operator(std::string expr) {
+    if (expr.length() != 1)
+        return (false);
     std::string operators = "+-*/";
-    for (int i = 0; i < operators.length(); i++) {
+    for (size_t i = 0; i < operators.length(); i++) {
         if (operators[i] == expr[0])
             return (true);
     }
     return (false);
 }
 
-// void RPN::evaluate(char *expr) {
-//     while (expr != NULL)
-//     {
-//         int left_operator;
-//         int right_operator;
-//         std::string current_expr = expr;
-//         expr = strtok(NULL, "-");
-//     }
-// }
+void RPN::evaluator(char *expr) {
+    pars_expr(expr);
+    while (expr != NULL)
+    { 
+        if (is_operator(expr)) {
+            std::string current_expr = expr;
+            if (my_stack.size() < 2 || current_expr.length() > 1) {
+                std::cout << "Error\n";
+                return;
+            }
+            int left_operator;
+            int right_operator;
+            right_operator = my_stack.top();
+            my_stack.pop();
+            left_operator = my_stack.top();
+            my_stack.pop();
+            my_stack.push(calculate(expr[0], left_operator, right_operator));
+        }
+       
+        else {
+            my_stack.push(atoi(expr));
+        }
+        expr = strtok(NULL, " ");
+    }
+    std::cout << my_stack.top() << std::endl;
+}
 
 RPN::~RPN() { }
