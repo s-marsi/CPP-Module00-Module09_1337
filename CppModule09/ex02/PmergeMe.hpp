80943d2ee78aaa2a14d4faab6918689f3a9e8352
Soufyane Marsi <smarsi@e1r12p3.1337.ma>
2025-06-21 14:18:45 +0100

    feat: add deque logique

--- CppModule09/ex02/PmergeMe.hpp
+++ CppModule09/ex02/PmergeMe.hpp
@@ -3,26 +3,40 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <deque>
 #include <iomanip>
+#include <climits>
+#include <ctime>
+
 typedef std::vector< std::pair<int, int> > pair_vector;
+typedef std::deque< std::pair<int, int> > pair_deque;
 
 class PmergeMe
 {
-    pair_vector pair_array;
+    pair_vector pair_vector;
+    pair_deque pair_deque;
+
     std::vector<int> largest;
     std::vector<int> lowest;
-    int leftover;
+
+    std::deque<int> largest_dq;
+    std::deque<int> lowest_dq;
+    
+        int leftover;
     private:
-        PmergeMe();
-        PmergeMe(PmergeMe& rhs);
-        PmergeMe& operator=(PmergeMe& rhs);
+    PmergeMe(PmergeMe& rhs);
+    PmergeMe& operator=(PmergeMe& rhs);
     public:
-        PmergeMe(int ac, char *av[]);
-        void  merge_insert_sort();
-        void  sort_pair();
-        void split_pairs();
+        PmergeMe();
+        void  merge_insert_sort_vector(int ac, char *av[]);
+        void  merge_insert_sort_deque(int ac, char *av[]);
+        void  args_to_deque(int ac, char *av[]) ;
+        void  args_to_vector(int ac, char *av[]) ;
+        void  push_sort_pair(int num1, int num2, std::string type);
+        void split_pairs(std::string type);
         ~PmergeMe();
 };
-void merge_sort(std::vector<int> &arr_to_sort, int left, int right);
-bool array_is_sorted(std::vector<int> &arr_to_sort);
+
+#include "merge_sort.tpp"
+
 #endif
