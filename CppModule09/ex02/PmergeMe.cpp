55385ede0129f73f8754b8220a05077d4213d8c4
Soufyane Marsi <smarsi@e1r12p3.1337.ma>
2025-06-21 15:33:02 +0100

    feat: handle sort using both deque and vector

--- CppModule09/ex02/PmergeMe.cpp
+++ CppModule09/ex02/PmergeMe.cpp
@@ -2,87 +2,6 @@
 
 PmergeMe::PmergeMe(): leftover(-1) {}
 
-void print_pair(pair_vector &to_print, int &leftover) {
-    pair_vector::iterator begin = to_print.begin();
-    pair_vector::iterator end = to_print.end();
-    while (begin != end) {
-        std::cout << " [" << (*begin).first << ", " << (*begin).second << "] |" ;
-        begin++;
-    }
-    std::cout << " leftover= " << leftover << std::endl;
-}
-
-void print_vector(std::vector<int> &to_print) {
-    std::vector<int>::iterator begin = to_print.begin();
-    std::vector<int>::iterator end = to_print.end();
-    while (begin != end) {
-        std::cout << " [" << std::setw(5) << *begin << "] |" ;
-        begin++;
-    }
-    std::cout << "\n";
-}
-
-
-void  PmergeMe::sort_pair()
-{
-    for(size_t i = 0; i < pair_array.size(); i++)
-    {
-        if (pair_array[i].first < pair_array[i].second) {
-            int tmp = pair_array[i].first;
-            pair_array[i].first = pair_array[i].second;
-            pair_array[i].second = tmp;
-        }
-    }
-}
-
-void  PmergeMe::split_pairs() {
-    for(size_t i = 0; i < pair_array.size(); i++)
-    {
-        largest.push_back(pair_array[i].first);
-        lowest.push_back(pair_array[i].second);
-    }
-}
-
-void  PmergeMe::merge_insert_sort()
-{
-    std::cout << std::setw(20) << "before sort: ";
-    print_pair(pair_array, leftover);
-    sort_pair();
-    std::cout << std::setw(20) << "after sort: ";
-    print_pair(pair_array, leftover);
-    split_pairs();
-    std::cout << std::setw(20) << "largest numbers: ";
-    print_vector(largest);
-    std::cout << std::setw(20) << "lowest numbers: ";
-    print_vector(lowest);
-
-    if (!array_is_sorted(largest))
-        merge_sort(largest, 0, largest.size() - 1);
-    print_vector(largest);
-    print_vector(lowest);
-
-}
-
-PmergeMe::PmergeMe(int ac, char *av[]): leftover(-1)
-{
-    for (int i = 1; i < ac; i++)
-    {
-        if (ac % 2 == 0)
-        {
-            if (i == ac - 1)
-                leftover = atoi(av[i]);
-            else {
-                pair_array.push_back(std::make_pair(atoi(av[i]), atoi(av[i + 1])));
-                i++;
-            }
-        }
-        else {
-                pair_array.push_back(std::make_pair(atoi(av[i]), atoi(av[i + 1])));
-                i++;
-        }
-    }
-}
-
 PmergeMe::PmergeMe(PmergeMe& rhs)
 { *this = rhs; }
 PmergeMe& PmergeMe::operator=(PmergeMe& rhs)
@@ -90,3 +9,113 @@ PmergeMe& PmergeMe::operator=(PmergeMe& rhs)
 return (*this); }
 PmergeMe::~PmergeMe()
 { }
+
+void  PmergeMe::push_sort_pair(int num1, int num2, std::string type)
+{
+    if (type == "vector") {
+        if (num1 <= num2)
+            pair_vector.push_back(std::make_pair(num1, num2));
+        else
+            pair_vector.push_back(std::make_pair(num2, num1));
+    }
+    else {
+        if (num1 <= num2)
+            pair_deque.push_back(std::make_pair(num1, num2));
+        else
+            pair_deque.push_back(std::make_pair(num2, num1));  
+    }
+}
+
+void  PmergeMe::split_pairs(std::string type) {
+    if (type == "vector")
+    {
+        for(size_t i = 0; i < pair_vector.size(); i++)
+        {
+            largest.push_back(pair_vector[i].second);
+            lowest.push_back(pair_vector[i].first);
+        }
+    }
+    else
+    {
+        for(size_t i = 0; i < pair_deque.size(); i++)
+        {
+            largest_dq.push_back(pair_deque[i].second);
+            lowest_dq.push_back(pair_deque[i].first);
+
+        }  
+    }
+}
+
+// ---------------------- Vector Logique ---------------
+
+void  PmergeMe::merge_insert_sort_vector(int ac, char *av[])
+{
+    std::clock_t start;
+	std::clock_t end;
+    start = std::clock();
+    args_to_vector(ac, av);
+    split_pairs("vector");
+    if (!array_is_sorted(largest))
+        merge_sort(largest, 0, largest.size() - 1);
+    for(size_t i = 0; i < lowest.size(); i++)
+    {
+        binary_search_insert(largest, lowest[i], 0, largest.size() - 1);
+    } 
+    end = std::clock();
+    // std::cout << "Before: "; print_STL(pair_vector); std::cout << ", " ; std::cout << std::endl;
+    std::cout << "After: "; print_STL(largest); std::cout << std::endl;
+    std::cout << "Time to process a range of " << largest.size() <<  " elements with std::vector :" << (end - start) << " us" << std::endl;
+}
+
+void  PmergeMe::args_to_vector(int ac, char *av[])
+{
+    for (int i = 1; i < ac; i++)
+    {
+        if (ac % 2 == 0 && i == ac - 1) {
+            args.push_back(atoi(av[i]));
+            leftover = atoi(av[i]);
+        }
+        else
+        {
+            args.push_back(atoi(av[i]));
+            args.push_back(atoi(av[i + 1]));
+
+            push_sort_pair(atoi(av[i]), atoi(av[i + 1]), "vector");
+            i++;
+        }
+    }
+}
+
+// ---------------------- Deque Logique ---------------
+
+void  PmergeMe::merge_insert_sort_deque(int ac, char *av[])
+{
+    std::clock_t start;
+	std::clock_t end;
+    start = std::clock();
+    args_to_deque(ac, av);
+    split_pairs("deque");
+    if (!array_is_sorted(largest_dq))
+        merge_sort(largest_dq, 0, largest_dq.size() - 1);
+    for(size_t i = 0; i < lowest_dq.size(); i++)
+    {
+        binary_search_insert(largest_dq, lowest_dq[i], 0, largest_dq.size() - 1);
+    } 
+    end = std::clock();
+    std::cout << "Time to process a range of " << largest_dq.size() <<  " elements with std::deque :" << (end - start) << " us" << std::endl;
+}
+
+void  PmergeMe::args_to_deque(int ac, char *av[])
+{
+    for (int i = 1; i < ac; i++)
+    {
+        if (ac % 2 == 0 && i == ac - 1)
+            leftover = atoi(av[i]);
+        else
+        {
+            push_sort_pair(atoi(av[i]), atoi(av[i + 1]), "deque");
+            i++;
+        }
+    }
+}
+
