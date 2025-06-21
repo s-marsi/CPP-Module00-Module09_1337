28bcc138e3bb02b41c7291fd15b4418ba0fbbee4
Soufyane Marsi <smarsi@e1r12p3.1337.ma>
2025-06-21 12:08:44 +0100

    fix: fix: handle numbers > INT_MAX and replace error prints with exceptions

--- CppModule09/ex02/main.cpp
+++ CppModule09/ex02/main.cpp
@@ -1,32 +1,45 @@
 #include "PmergeMe.hpp"
 
-void print_error_exit()
-{ std::cout << "Error" <<std::endl; exit (1); }
-
 void parse_args(int ac, char *av[])
 {
+    std::size_t tmp;
     for (int i = 1; i < ac; i++)
     {
         if (!av[i][0])
-        {
-            print_error_exit();
-        }
+            throw std::runtime_error("Error: bad args");
+
         for (size_t j = 0; j < std::string(av[i]).length(); j++) {
             if (!std::isdigit(av[i][j])){
-                print_error_exit();
+                throw std::runtime_error("Error: bad args");
             }
         }
+
+        tmp = atoll(av[i]);
+
+        if (tmp > INT_MAX)
+            throw std::runtime_error("Error: bad args");
     }
 }
 
 
 int main(int ac, char *av[])
 {
-    if (ac < 2) {
-        print_error_exit();
+    try
+    { 
+        if (ac < 2)
+            throw std::runtime_error("Error: bad args");
+
+        parse_args(ac, av);
+            
+        PmergeMe test;
+        test.merge_insert_sort();
     }
-    parse_args(ac, av);
-    PmergeMe test(ac, av);
-    test.merge_insert_sort();
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+   
+
     return (0);
 }
